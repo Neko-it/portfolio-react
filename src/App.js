c4480ba1f4c96347f1e5a2ef13aae0a23fb74ee4
Neko-it <115939884+Neko-it@users.noreply.github.com>
2022-11-18 16:27:13 +0100

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Routes,Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Tmp from './components/Work';
+import Esp from './components/Esp';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='esp' element={<Esp/>}/>
+        <Route path='code' element={<Tmp/>}/>
+        <Route path='contact' element={<Contact/>}/>
+      </Route>
+    </Routes>
+    </>
   );
 }
 
